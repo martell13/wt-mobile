9ae4ac58050ab301463c6a51071e508ad04dbfbd
martell13 <166794223+martell13@users.noreply.github.com>
2025-09-09 18:10:48 +0000

    git add vite.config.ts git commit -m "chore: set Vite base for GitHub Pages" git push

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // 👇 use your repo name here
   base: '/wt-mobile/',
   plugins: [react()],
 })
